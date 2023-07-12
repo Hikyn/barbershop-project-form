4a590d469edc25b87b4a09c86433c08a7157f752
Danila <danila10danila@mail.ru>
2023-07-12 13:15:38 +0300

    add service categories

--- src/components/ServicesForm.tsx
+++ src/components/ServicesForm.tsx
@@ -1,4 +1,4 @@
-import '../styling/Location.scss';
+import '../styling/ServicesForm.scss';
 import { FormEventHandler, useEffect, useState } from 'react';
 
 interface Service {
@@ -6,6 +6,7 @@ interface Service {
     name: string;
     price: number;
     duration: number;
+    category: string;
   }
 
 interface Props {
@@ -14,6 +15,11 @@ interface Props {
 
 const ServicesForm: React.FC<Props> = ({ setFormServices }) => {
   const [availableServices, setAvailableServices] = useState<Service[]>([]);
+  const [hairServices, setHairServices] = useState<Service[]>([]);
+  const [beardServices, setBeardServices] = useState<Service[]>([]);
+  const [facialServices, setFacialServices] = useState<Service[]>([]);
+  const [shaveServices, setShaveServices] = useState<Service[]>([]);
+  const [packageServices, setPackageServices] = useState<Service[]>([]);
   // This component will render 5 different form components, all of them will change state.
   function onSubmitButton(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -29,27 +35,94 @@ const ServicesForm: React.FC<Props> = ({ setFormServices }) => {
     
     setFormServices(selected_services);
   };
-  async function getServices(){
-    const url = new URL("http:/localhost:3000/services");
-    const res = await fetch(url, {
-      method: "GET"
-    })
-    const data = await res.json();
-    //console.log(data)
-    console.log("Requesting services")
-    setAvailableServices(data);
-  }
+
   useEffect(() => {
+    // On mount request services from REST api
+    async function getServices(){
+      const url = new URL("http:/localhost:3000/services");
+      const res = await fetch(url, {
+        method: "GET"
+      })
+      const data = await res.json();
+      //console.log(data)
+      console.log("Requesting services")
+      setAvailableServices(data);
+    }
+
     getServices();
   }, [])
+
+  useEffect(() => {
+    // Hook for updating services categories when we finally receive them from REST api
+    function getServicesCategories() {
+      let hairArray: Service[] = [];
+      let beardArray: Service[] = [];
+      let facialArray: Service[] = [];
+      let shaveArray: Service[] = [];
+      let packageArray: Service[] = [];
+      availableServices.forEach((service) => {
+        if (service.category === "Hair") {
+          hairArray.push(service);
+        } else if (service.category === "Beard") {
+          beardArray.push(service);
+        } else if (service.category === "Facial Treatment") {
+          facialArray.push(service);
+        } else if (service.category === "Shave") {
+          shaveArray.push(service);
+        } else if (service.category === "Packages & Combos") {
+          packageArray.push(service);
+        }
+      })
+      setHairServices(hairArray);
+      setBeardServices(beardArray);
+      setFacialServices(facialArray);
+      setShaveServices(shaveArray);
+      setPackageServices(packageArray);
+    }
+
+    getServicesCategories();
+  }, [availableServices])
 
   return (
     <form className="locationForm" onSubmit={onSubmitButton}>
         <h1 className='sectionAnnounce'>Select services: </h1>
-        {availableServices.map((service) => {
+        <h2>Hair</h2>
+        {hairServices.map((service) => {
             return (
                 <div key={"service" + service._id}>
-                    <input type='checkbox' className='checkbox' id={'service' + service.name} name='service' value={service._id}></input>
+                    <input type='checkbox' className='checkbox-round' id={'service' + service.name} name='service' value={service._id}></input>
+                    <label htmlFor={'service' + service.name}>{service.name}</label>
+                </div>
+        )})}
+        <h2>Beard</h2>
+        {beardServices.map((service) => {
+            return (
+                <div key={"service" + service._id}>
+                    <input type='checkbox' className='checkbox-round' id={'service' + service.name} name='service' value={service._id}></input>
+                    <label htmlFor={'service' + service.name}>{service.name}</label>
+                </div>
+        )})}
+        <h2>Facial Treatment</h2>
+        {facialServices.map((service) => {
+            return (
+                <div key={"service" + service._id}>
+                    <input type='checkbox' className='checkbox-round' id={'service' + service.name} name='service' value={service._id}></input>
+                    <label htmlFor={'service' + service.name}>{service.name}</label>
+                </div>
+        )})}
+        <h2>Shave</h2>
+        {shaveServices.map((service) => {
+            return (
+                <div key={"service" + service._id}>
+                    <input type='checkbox' className='checkbox-round' id={'service' + service.name} name='service' value={service._id}></input>
+                    <label htmlFor={'service' + service.name}>{service.name}</label>
+                </div>
+        )})}
+        <h2>Packages & Combos</h2>
+        {packageServices.map((service) => {
+            return (
+                <div key={"service" + service._id}>
+                    <input type='checkbox' className='checkbox-round' id={'service' + service.name} name='service' value={service._id}></input>
                     <label htmlFor={'service' + service.name}>{service.name}</label>
                 </div>
         )})}
